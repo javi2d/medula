63ddb30b9e3eed2a53d78385007d1427d237f68a
Javi <javi2d@gmail.com>
2011-12-15 19:46:36 +0100

    Clean 1

--- local/_init.py
+++ local/_init.py
@@ -1,10 +1,32 @@
 
 
-#local = sop.Expose.object( medula( '../myCompanyName' ) , 'local' )
-# redefine local to keep your personal data outside the medula folder structure.
+local_config =  None # '../myCompanyName' or absolute paths  /Volumes/Disk/myCompanyName
+
+main_toolset = 'main'
 
 
-medula.local.main._init()
-# Launch the sample pipeline  [ medula/local/main ]
+'''
+local_config 
+
+	redefine this variable to change the local folder
+
+main_toolset
+	
+	redefine this variable to change the main toolset
+
+'''
+
+
+if local_config:
+	
+	local = sop.Expose.object( medula( local_config ) , 'local' ) 
+
+
+local( "%s/_init.py" % main_toolset )()
+
+
+
+
+
 
 
