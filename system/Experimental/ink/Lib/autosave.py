5014a0c051b3bcec6f261137c079fdba0492e853
Javi <javi2d@gmail.com>
2011-12-23 12:54:56 +0100

    Added ink

---
+++ system/Experimental/ink/Lib/autosave.py
@@ -0,0 +1,66 @@
+
+
+
+def _digest( content ):
+
+	import hashlib
+	md5 = hashlib.md5()
+	md5.update( content )
+	return md5.digest()
+
+
+def save( space ):
+
+
+	actual_code = space['read']
+
+	last_saved , md5digest = brain( 'autosave' , ( time.time() , _digest( actual_code ) ) )
+
+	#if ( time.time() - last_saved ) > brain( 'autosave_timeout' , 30 ):
+	
+	if not md5digest == _digest( actual_code ):
+	
+		#space['backup'] 
+		
+		save_history( space )
+		
+		brain.autosave = time.time() , _digest( actual_code )
+	
+		return True
+	
+		
+	return False
+	
+
+def save_history( space ):
+	
+	history_path = ink( 'History/%s' % '_'.join(  [ str(x) for x in  time.localtime()[:3] ] ) )['$PATH']
+	
+	import shutil
+
+	idx = 0
+
+	while 1:
+	
+		backup_file = '%s.%04d' % ( space['file'] , idx )
+		
+		backup_file = Normalize.join( history_path ,  os.path.basename( backup_file ) )
+		
+		if not os.path.isfile( backup_file ):
+			
+			#print backup_file
+			
+			args = space['file'] , backup_file
+			
+			shutil.copy( *args )
+				
+			break
+	
+		else:
+		
+			idx += 1
+			
+	
+	
+	
+
---
+++ system/Experimental/ink/ink.py
@@ -0,0 +1,143 @@
+
+import sys
+try: sys.dont_write_bytecode = True
+except: pass
+
+
+
+sleep_time = .1
+
+if __name__ == '__main__':
+	
+	# Evaluated in main
+
+	import sop_finder
+	import sop	
+
+
+	sop.Expose.object( sh( sop.sys.argv[0] )['shell'] , 'ink' )
+		
+	brain.Lib << ink.Lib
+	
+	ink( 'Sources/ink_source.py' )
+	
+	raw_input( """
+
+Ink; I(nteractive)n(u)k(e) is an experimental File Interpreter.
+
+Edit and save 'ink/Sources/ink_source.py' file.
+	
+Press a key to start interpreting ...""" )
+	
+	
+	while 1:
+		
+		sop.Core.execution( space['file'] )
+
+
+else:
+		
+	# This code is evaluated in a new Space each time
+	
+	brain( '_sleep_time' , .1 )
+
+	active = False
+	
+	source = ink( 'Sources/ink_source.py' )
+
+	stats = brain( 'stats' , '' )
+	
+	new_stats = os.stat( source['file'] )[-2:]
+	
+	if not stats == new_stats:
+		
+		active = True
+		
+		brain.loop_stats_id  = loop_stats_id = brain( 'loop_stats_id' , 0 ) + 1
+		
+		print '\n'*10
+		print '| %s | ink execution loop unit.' % brain.loop_stats_id
+		print '_'*50
+		print '\n\n'
+		
+		brain.ink << ink( 'ink.memory' )
+		
+		#print brain
+		
+		brain( 'SHARED_CONTEXT' , Space() )
+		
+		
+		
+		use_shared_context = brain.ink( 'USE_SHARED_CONTEXT' , True )
+		
+		#print 'Control' , time.time() , use_shared_context
+		#print brain.ink
+		
+		if not use_shared_context:
+			
+			brain.SHARED_CONTEXT = Space()
+			
+			print 'renewed context'
+			
+		
+		
+		
+		stored_history = False
+		
+		try:
+			
+			sop.Core.execution( source['file'] , brain.SHARED_CONTEXT )
+			
+			#brain.Lib << ink.Lib
+			
+			if brain.ink( 'STORE_SCRIPT_HISTORY' , True ):
+				
+				stored_history = brain.Lib.autosave.save( source )
+			
+			
+			
+		except:
+			
+			if brain.ink( 'STORE_IN_ERROR_CODE' , False ):
+				
+				stored_history = brain.Lib.autosave.save( source )
+			
+			
+			raise
+		
+		
+		finally:
+			
+			if stored_history:
+			
+				print '\n\n... script history stored' 
+		
+		
+		
+		brain.stats = new_stats
+		
+		#print '\n\n>>'
+
+	else:
+		
+		time.sleep( brain._sleep_time )
+	
+	
+	if active:
+		
+		brain._sleep_time = .01
+		active = False
+		
+	else:
+		
+		if brain._sleep_time < 5:
+
+			brain._sleep_time += 0.01
+		
+		#print brain._sleep_time
+
+
+
+	
+		
+
